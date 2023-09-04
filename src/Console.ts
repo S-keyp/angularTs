3f59df25fc65dbea858d23e685e9c5988553a1a9
matteokeole <matteo@studiokg.fr>
2023-09-04 16:14:54 +0200

    feat: Console

---
+++ src/Console.ts
@@ -0,0 +1,80 @@
+import {question} from "readline-sync";
+
+type ConsoleOptions = {[key: string]: Function|null};
+
+interface ConsoleInterface {
+    isActive(): boolean;
+    setActive(active: boolean): void;
+    getOptions(): ConsoleOptions;
+    setOptions(options: ConsoleOptions): void;
+    error(message: string): void;
+    trace(message: string): void;
+    warn(message: string): void;
+    listen(): void;
+    clear(): void;
+    exit(): void;
+}
+
+export class Console implements ConsoleInterface {
+    private active: boolean;
+    private options: ConsoleOptions;
+
+    constructor() {
+        this.active = true;
+        this.options = {};
+
+        this.clear();
+    }
+
+    isActive(): boolean {
+        return this.active;
+    }
+
+    setActive(active: boolean): void {
+        this.active = active;
+    }
+
+    getOptions(): ConsoleOptions {
+        return this.options;
+    }
+
+    setOptions(options: ConsoleOptions): void {
+        this.options = options;
+    }
+
+    private print(message: string): void {
+        process.stdout.write(message);
+    }
+
+    error(message: string): void {
+        this.print(`\x1b[93m${message}\x1b[39m`);
+    }
+
+    trace(message: string): void {
+        this.print(message);
+    }
+
+    warn(message: string): void {
+        this.print(`\x1b[93m${message}\x1b[39m`);
+    }
+
+    listen(): void {
+        const input = question();
+        const option: Function|null = this.options[input] ?? null;
+
+        if (option === null) {
+            this.warn(`Invalid option '${input}'.\n`);
+        } else option();
+
+        this.trace("\n");
+    }
+
+    clear(): void {
+        process.stdout.cursorTo(0, 0);
+        process.stdout.clearScreenDown();
+    }
+
+    exit(): void {
+        this.active = false;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,22 @@
-import * as readlineSync from "readline-sync";
-import axios from "axios";
+import {Console} from "./Console";
 
-console.clear();
+const console: Console = new Console();
 
-console.log("** Administration Collegues **");
-console.log("1. Lister les collègues");
-console.log("99. Sortir");
+console.setOptions({
+    "1": function(): void {
+        /** @todo C'est pour toi Vic :p */
+    },
+    "2": function(): void {
+        console.trace("Bye");
+        console.exit();
+    },
+});
 
-// Stocker la liste des collègues
-let collegues: any[] = [];
+while (console.isActive()) {
+    console.trace("--- Colleague Administration ---\n\n");
+    console.trace("1. List colleagues\n");
+    console.trace("2. Exit\n");
+    console.trace("\n> ");
 
-// URL de l'API à appeler pour lister les collègues
-const API_all_colleagues =
-  "https://app-6f6e9c23-7f63-4d86-975b-a0b1a1440f94.cleverapps.io/api/v2/colleagues";
-
-// Fonction asynchrone pour récupérer la liste des collègues
-async function fetchCollegues() {
-  try {
-    const response = await axios.get(API_all_colleagues);
-    // Traitez ici la réponse de l'API
-    collegues = response.data;
-  } catch (error) {
-    console.error("Erreur lors de la récupération des collègues :", error);
-  }
+    console.listen();
 }
-
-// Fonction principale asynchrone
-async function main() {
-  // Attendre que la liste des collègues soit récupérée
-  await fetchCollegues();
-
-  while (true) {
-    // Saisie utilisateur
-    const reponse = readlineSync.question("Votre choix : ");
-
-    // Si la réponse est "99", on sort
-    if (reponse === "99") {
-      console.log("Au revoir");
-      break;
-    }
-
-    // Si la réponse est "1", on affiche la liste des collègues
-    if (reponse === "1") {
-      console.log(">> Liste des collègues");
-
-      // Afficher les informations de chaque collègue
-      for (const collegue of collegues) {
-        console.log("Pseudo :", collegue.pseudo);
-        console.log("Photo :", collegue.photo);
-        console.log("Score :", collegue.score);
-        console.log("\n");
-      }
-    } else {
-      console.log("Choix invalide, veuillez entrer 1 ou 99.");
-    }
-  }
-}
-
-// Appeler la fonction principale asynchrone pour démarrer le programme
-main();
